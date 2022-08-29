4d05ed1e3fc9eda1781018e4deccde1d5e6e80e1
john <johnsloan1984@gmail.com>
2022-08-29 10:49:46 +0100

    added AbRadioGroupProps to export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import AbButton from "./components/AbButton";
 import AbRadioGroup from "./components/AbRadioGroup";
 import AbClickableCard from "./components/AbClickableCard";
+import { AbRadioGroupProps } from "./components/AbRadioGroup/types";
 
 export { AbButton, AbClickableCard, AbRadioGroup };
+export type { AbRadioGroupProps };
